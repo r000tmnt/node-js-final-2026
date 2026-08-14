7d6d9c57712d556f3a81af43d90f87de0cbdfb6f
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-14 17:08:15 +0800

    [Update] 接上 skill 和 creditPackage router

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-
 const { dataSource } = require('./db/data-source');
+const appError = require('./utils/appError');
+const skillRouter = require('./router/skill')
+const creditPackageRouter = require('./router/creditPackage')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/coaches/skill', skillRouter)
+app.use('/api/credit-package', creditPackageRouter)
 
 
 app.get("/healthcheck", async (req, res) => {
@@ -19,7 +24,8 @@ app.get("/healthcheck", async (req, res) => {
 
 // 404
 app.use((req, res, next) => {
-    res.status(404).json({ status: 'failed', message: 'Not Found' });
+    // res.status(404).json({ status: 'failed', message: 'Not Found' });
+    next(appError(404, 'Not found'))
     return
 })
 
