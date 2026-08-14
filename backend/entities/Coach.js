86314f8d7ae034456d1e14f029da7b2a309083b1
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-14 11:23:05 +0800

    [Update] Coach 設計圖

---
+++ backend/entities/Coach.js
@@ -0,0 +1,47 @@
+const { EntitySchema } = require('typeorm');
+
+module.exports = new EntitySchema({
+    name: 'Coach',
+    tableName: 'COACH',
+    columns: {
+        id: {
+            primary: true,
+            type: 'uuid',
+            generated: 'uuid',
+        },
+        user_id: {
+            type: 'uuid',
+            nullable: false,
+            unique: true,
+        },
+        experience_year: {
+            type: 'int',
+            nullable: false,
+            default: 0,
+        },
+        description: {
+            type: 'text',
+            nullable: true,
+        },
+        profile_image_url: {
+            type: 'varchar',
+            length: 2048,
+            nullable: true,
+        },
+        created_at: {
+            type: 'timestamp',
+            createDate: true,
+        },
+        updated_at: {
+            type: 'timestamp',
+            updateDate: true,
+        }
+    },
+    relations: {
+        user: {
+            target: 'User',
+            type: 'one-to-one',
+            joinColumn: { name: 'user_id' },
+        }
+    }
+})
