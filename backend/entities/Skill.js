70123bac81f1b96ea00badd1b053438290171d00
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-14 11:23:23 +0800

    [Update] Skill 設計圖

---
+++ backend/entities/Skill.js
@@ -0,0 +1,18 @@
+const { EntitySchema } = require('typeorm');
+
+module.exports = new EntitySchema({
+    name: 'Skill',
+    tableName: 'SKILL',
+    columns: {
+        id: {
+            primary: true,
+            type: 'uuid',
+            generated: 'uuid',
+        },
+        name: {
+            type: 'varchar',
+            length: 20,
+            nullable: false,
+        }
+    }
+})
