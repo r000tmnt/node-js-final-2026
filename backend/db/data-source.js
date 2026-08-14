d1792460ce809947a4f403d5afd7b7307e570c6e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-14 11:22:45 +0800

    [Update] 資料庫連線與引用 entities

---
+++ backend/db/data-source.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const { DataSource } = require('typeorm')
+
+const Coach = require('../entities/Coach')
+const Skill = require('../entities/Skill')
+const User = require('../entities/User')
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  ssl: process.env.DB_ENABLE_SSL === 'true',
+  entities: [
+    // 今天的 entities 會一個一個長出來
+    Coach,
+    Skill,
+    User
+  ],
+})
+
+module.exports = { dataSource }
