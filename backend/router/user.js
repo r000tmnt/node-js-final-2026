73dce38f30dd279897ea951a9f1e17a8172b08a6
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-17 20:03:49 +0800

    [Update] 補上取得 profile 和更新與變更密碼的內容

--- backend/router/user.js
+++ backend/router/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const appError = require('../utils/appError')
 const userController = require('../controllers/userController');
+const isAuth = require('../middleware/isAuth')
 
 const router = express.Router();
 
@@ -34,24 +35,58 @@ router.post('/login', async(req, res, next) => {
     }
 })
 
-router.get('/profile', async(req, res, next) => {
+router.get('/profile', isAuth, async(req, res, next) => {
     try {
-        // const { id } = req.params
-        // const result = await skillController.deleteSkill(id)
+        const { user } = req
+        const result = await userController.getUser(user)
 
-        // if(result.status !== 'success'){
-        //     next(result)
-        //     return   
-        // }
+        if(result.status !== 'success'){
+            next(result)
+            return   
+        }
 
-        // res.status(200).json(result) 
+        res.status(200).json(result) 
     } catch (error) {
         next(appError(503, error))
     }
 })
 
-router.put('/profile', async(req, res, next) => {})
+router.put('/profile', isAuth, async(req, res, next) => {
+    try {
+        const { name } = req.body
+        const result = await userController.updateUser({ user: req.user, name })
 
-router.put('/password', async(req, res, next) => {})
+        if(result.status !== 'success'){
+            next(result)
+            return   
+        }
+
+        res.status(200).json(result)         
+    } catch (error) {
+        console.log(error)
+        next(appError(400, '更新使用者資料失敗'))   
+    }
+})
+
+router.put('/password', isAuth, async(req, res, next) => {
+    try {
+        const { password, new_password, confirm_new_password } = req.body
+                const result = await userController.changePwd({ 
+                    user: req.user, 
+                    password, 
+                    new_password, confirm_new_password 
+                })
+
+        if(result.status !== 'success'){
+            next(result)
+            return   
+        }
+
+        res.status(200).json(result)            
+    } catch (error) {
+        console.log(error)
+        next(appError(400, '更新使用者資料失敗'))   
+    }
+})
 
 module.exports = router
