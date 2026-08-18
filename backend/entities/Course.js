bca6b9e11fd08586e265cf058fb832fa167b3ef3
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-18 10:44:55 +0800

    [Create] Course 設計圖

---
+++ backend/entities/Course.js
@@ -0,0 +1,52 @@
+const { EntitySchema } = require('typeorm')
+
+module.exports = new EntitySchema({
+    name: 'Course',
+    tableName: 'COURSE',
+    columns: {
+        id: {
+            primary: true,
+            type: 'uuid',
+            generated: 'uuid',
+        },
+        name: {
+            type: 'varchar',
+            length: 50,
+            nullable: false,
+        },
+        status: {
+            type: 'varchar',
+            length: 20,
+            nullable: false
+        },
+        start_at: {
+            type: 'timestamp',
+            nullable: false
+        },
+        end_at: {
+            type: 'timestamp',
+            nullable: false
+        },
+        max_participants: {
+            type: 'int',
+            nullable: false
+        },
+        meeting_url: {
+            type: 'varchar',
+            length: 100,
+            nullable: true
+        },
+        participants: {
+            type: 'int',
+            nullable: false
+        },
+        created_at: {
+            type: 'timestamp',
+            createDate: true,
+        },
+        updated_at: {
+            type: 'timestamp',
+            updateDate: true,
+        }        
+    }
+})
