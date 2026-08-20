6be8d08356d5c65c4f952201f439b313df67484a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-20 17:18:15 +0800

    [Update] 新增教練課程與調整路由讀取順序

--- backend/router/coach.js
+++ backend/router/coach.js
@@ -8,6 +8,45 @@ const coachController = require('../controllers/coachController')
 
 const router = express.Router()
 
+router.get('/courses', isAuth, isCoach, async(req, res, next) => {
+    try {
+        const { user } = req
+        
+        const result = await coachController.getCourses(user.id)
+
+        if(result.status !== 'success'){
+            next(result)
+            return
+        }
+
+        res.status(200).json(result)  
+    } catch (error) {
+        console.log(error)
+        next(appError(503, error))
+        return 
+    }
+})
+
+router.post('/courses', isAuth, isCoach, async(req, res, next) => {
+    try {
+        const { user } = req
+        
+        const result = await coachController.addCourse({user_id: user.id, ...req.body})
+
+        if(result.status !== 'success'){
+            next(result)
+            return
+        }
+
+        res.status(201).json(result)  
+    } catch (error) {
+        console.log('router.post /courses', error)
+        next(appError(503, error))
+        return 
+    }
+})
+
+
 router.post('/:user_id', async(req, res, next) => {
     try {
         const { user_id } = req.params
@@ -64,29 +103,6 @@ router.put('/', isAuth, isCoach, async(req, res, next) => {
         return 
     }
 })
-
-router.get('/courses', isAuth, isCoach, async(req, res, next) => {
-    try {
-        const { user } = req
-        
-        const result = await coachController.getCourses(user.id)
-
-        if(result.status !== 'success'){
-            next(result)
-            return
-        }
-
-        res.status(200).json(result)  
-    } catch (error) {
-        console.log(error)
-        next(appError(503, error))
-        return 
-    }
-})
-
-// router.post('/courses', isCoach, async(req, res, next) => {
-
-// })
 
 // router.get('/courses/:course_id', isCoach, async(req, res, next) => {
 
