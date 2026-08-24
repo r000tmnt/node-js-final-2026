ded0342012c7c97b687ab2a9fce60034e4f09c06
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-24 18:34:47 +0800

    [Update] 公開查詢單一教練資訊

--- backend/controllers/publicController.js
+++ backend/controllers/publicController.js
@@ -1,5 +1,6 @@
 const { dataSource } = require("../db/data-source")
 const appError = require("../utils/appError")
+const { isValidString } = require("../utils/validator")
 
 const publicController = {
     getCoaches: async({ per, page }) => {
@@ -20,7 +21,37 @@ const publicController = {
             status: 'success',
             data: result[0]
         }
-    }
+    },
+    
+    getCoach: async(coach_id) => {
+        if(!isValidString(coach_id)){
+            return appError(400, '欄位未填寫正確')
+        }
+
+        const coach = await dataSource.getRepository('Coach').findOneBy({ 
+            id: coach_id
+         })
+
+        if(!coach){
+            return appError(400, '找不到該教練')
+        }
+
+        coach['skills'] = []
+
+        const skills = dataSource.getRepository('Skill')
+
+        for(let i=0; i < coach.skill_ids.length; i++){
+            const skill = await skills.findOneBy({ id: coach.skill_ids[i] })
+            coach['skills'].push(skill.name)
+        }
+
+        delete coach.skill_ids
+
+        return {
+            status: 'success',
+            data: coach
+        }
+    },
 }
 
 module.exports = publicController
--- backend/router/public.js
+++ backend/router/public.js
@@ -21,7 +21,22 @@ router.get('/coaches', async(req, res, next) => {
     }
 })
 
-// router.get('/coaches/:coach_id', async(req, res, next) => {})
+router.get('/coaches/:coach_id', async(req, res, next) => {
+    try {
+        const { coach_id } = req.params
+        const result = await publicController.getCoach(coach_id)
+        
+        if(result.status !== 'success'){
+            next(result)
+            return
+        }
+
+        res.status(200).json(result)          
+    } catch (error) {
+        next(appError(503, error))
+        return         
+    }
+})
 
 // router.get('/coaches/:coach_id/courses', async(req, res, next) => {})
 
