cf5c2a0bcedc92e1e3b221118a55a75222ec6ff0
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-25 10:10:46 +0800

    [Update] 購買方案移動到 creditPackageController

--- backend/controllers/creditPackageController.js
+++ backend/controllers/creditPackageController.js
@@ -57,7 +57,27 @@ const creditPackageController = {
             status: 'success',
             data: result
         }
-    }
+    },
+
+    purchase: async({ user_id, credit_package_id }) => {
+        const package = await dataSource.getRepository('CreditPackage').findOneBy({ id: credit_package_id })
+
+        if(!package){
+            return appError(400, 'ID錯誤')
+        }
+
+        const result = await dataSource.getRepository('CreditPurchased').save({
+            user_id,
+            credit_package_id,
+            purchased_credits: package.credit_amount,
+            price_paid: package.price
+        })
+
+        return {
+            status: 'success',
+            data: null
+        }
+    }    
 }
 
 module.exports = creditPackageController
