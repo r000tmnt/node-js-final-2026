5cf35bcc9390c23b3ce5239353616dc0a4d8fa82
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-25 14:25:12 +0800

    [Update] 報名課程

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,6 +1,53 @@
+const appError = require('../utils/appError')
+const { dataSource } = require('../db/data-source')
+const { isValidString } = require('../utils/validator')
+
 const bookingController = {
-    booking: async() => {
+    booking: async({ user_id, course_id }) => {
+        const course = await dataSource.getRepository('Course').findOneBy({ id: course_id })
+
+        if(!course){
+            return appError(400, 'ID錯誤')
+        }
+
+        const courseBooking = await dataSource.getRepository('CourseBooking')
         
+        const booked = await courseBooking.findOneBy({ user_id, course_id })
+
+        if(booked){
+            return appError(400, '已經報名過此課程')
+        }
+
+        const purchasedAll = await dataSource.getRepository('CreditPurchased').find({ 
+            where: { user_id },
+            relations: {
+                creditPackage: true
+            } 
+        })
+
+        const totalCredits = purchasedAll.reduce((pre, cur) => {
+            pre + cur.creditPackage.credit_amount
+        }, 0)
+
+        const bookedAll = await courseBooking.findBy({ course_id })
+
+        if((totalCredits - bookedAll.filter(b => !b.cancelled_at).length) <= 0){
+            return appError(400, '已無可使用堂數')
+        }
+
+        if(bookedAll.length === course.max_participants){
+            return appError(400, '已達最大參加人數，無法參加')
+        }
+
+        const result = await courseBooking.save({
+            user_id,
+            course_id
+        })
+
+        return {
+            status: 'success',
+            data: null
+        }
     }
 }
 
--- backend/router/booking.js
+++ backend/router/booking.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const appError = require('../utils/appError')
 const isAuth = require('../middleware/isAuth')
+const bookingController = require('../controllers/bookingController')
 
 const router = express.Router()
 
 router.post('/:courseId', isAuth, async(req, res, next) => {
+    try {
+        const { courseId } = req.params
+        const result = await bookingController.booking({ user_id: req.user.id, course_id: courseId })
 
+        if(result.status !== 'success'){
+            next(result)
+            return
+        }
+
+        res.status(201).json(result)  
+    } catch (error) {
+        console.log(error)
+        next(appError(503, error))
+        return 
+    }
 })
 
-router.delete('/:courseId', isAuth, async(req, res, next) => {
+// router.delete('/:courseId', isAuth, async(req, res, next) => {
 
-})
+// })
 
 module.exports = router
