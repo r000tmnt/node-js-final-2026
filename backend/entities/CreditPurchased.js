61e2a8a4131753b4187ac657ce79e2d28cba93ca
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-25 10:01:12 +0800

    [Create] CreditPurchased 設計圖

---
+++ backend/entities/CreditPurchased.js
@@ -0,0 +1,52 @@
+const { EntitySchema } = require('typeorm')
+
+module.exports = new EntitySchema({
+    name: 'CreditPurchased',
+    tableName: 'CREDIT_PURCHASED',
+    columns: {
+        id: {
+            type: 'uuid',
+            primary: true,
+            generated: 'uuid'
+        },
+        user_id: {
+            type: 'uuid',
+            unique: false,
+            nullable: false
+        },
+        credit_package_id: {
+            type: 'uuid',
+            unique: false,
+            nullable: false
+        },        
+        name: {
+            type: 'varchar',
+            length: 50,
+            nullable: false
+        },
+        credit_amount: {
+            type: 'int',
+            nullable: false
+        },
+        price_paid: {
+            type: 'int',
+            nullable: false
+        },   
+        purchase_at: {
+            type: 'timestamp',
+            createDate: true
+        }     
+    },
+    relations: {
+        user: {
+            target: 'User',
+            type: 'many-to-one',
+            joinColumn: { name: 'user_id' }
+        },
+        creditPackage: {
+            target: 'CreditPackage',
+            type: 'many-to-one',
+            joinColumn: { name: 'credit_package_id' }
+        },        
+    }
+})
