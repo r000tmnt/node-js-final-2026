90cf00ed34bf98e6a6b504d084b97573f4616d88
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-25 10:00:49 +0800

    [Create] CourseBooking 設計圖

---
+++ backend/entities/CourseBooking.js
@@ -0,0 +1,39 @@
+const { EntitySchema } = require('typeorm')
+
+module.exports = new EntitySchema({
+    name: 'CourseBooking',
+    tableName: 'COURSE_BOOKING',
+    columns: {
+        id: {
+            type: 'uuid',
+            primary: true,
+            generated: 'uuid'
+        },
+        user_id: {
+            type: 'uuid',
+            unique: false,
+            nullable: false
+        },
+        course_id: {
+            type: 'uuid',
+            unique: false,
+            nullable: false
+        },
+        cancelled_at: {
+            type: 'timestamp',
+            nullable: true
+        }     
+    },
+    relations: {
+        user: {
+            target: 'User',
+            type: 'many-to-one',
+            joinColumn: { name: 'user_id' }
+        },
+        course: {
+            target: 'Course',
+            type: 'many-to-one',
+            joinColumn: { name: 'course_id' }
+        },        
+    }
+})
